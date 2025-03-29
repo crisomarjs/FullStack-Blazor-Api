547b3e3d22f348845a596589b9449a90b1c2868a
crisomarjs <cristianomar71@outlook.com>
2025-03-28 22:40:22 -0600

    Se crea el vista crear post

---
+++ ClienteBlazor/Pages/Posts/CreatePost.razor.cs
@@ -0,0 +1,26 @@
+﻿using ClienteBlazor.Helpers;
+using ClienteBlazor.Models;
+using ClienteBlazor.Services.IServices;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ClienteBlazor.Pages.Posts
+{
+    public partial class CreatePost
+    {
+        [Inject]
+        IPostService postService { get; set; }
+        [Inject]
+        IJSRuntime runtimeService { get; set; }
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+        private Post CrearPost { get; set; } = new Post();
+
+        private async Task OnCreatePost()
+        {
+            var createPost = await postService.CreatePost(CrearPost);
+            await runtimeService.ToastrSuccess("Post creado correctamente");
+            navigationManager.NavigateTo("posts");
+        }
+    }
+}
