b9f7e0683bc0ff9d66478424c6dc9a2f6f002344
crisomarjs <cristianomar71@outlook.com>
2025-03-31 14:25:39 -0600

    Creacion del la pantlla princiapl con el listado de los posts

---
+++ ClienteBlazor/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿using ClienteBlazor.Models;
+using ClienteBlazor.Services.IServices;
+using Microsoft.AspNetCore.Components;
+
+namespace ClienteBlazor.Pages
+{
+    public partial class Home
+    {
+        private IEnumerable<Post> Posts { get; set; } = new List<Post>();
+        [Inject]
+        IPostService postService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Posts = await postService.GetPosts();
+        }
+    }
+}
---
+++ ClienteBlazor/Pages/Posts/DetailPost.razor.cs
@@ -0,0 +1,20 @@
+﻿using ClienteBlazor.Models;
+using ClienteBlazor.Services.IServices;
+using Microsoft.AspNetCore.Components;
+
+namespace ClienteBlazor.Pages.Posts
+{
+    public partial class DetailPost
+    {
+        private Post post { get; set; } = new Post();
+        [Inject]
+        IPostService postService { get; set; }
+        [Parameter]
+        public int? Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            post = await postService.GetPost(Id.Value);
+        }
+    }
+}
